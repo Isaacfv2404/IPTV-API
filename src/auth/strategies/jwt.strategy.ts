2face428c30e4c9e270912bdf04f646c9d74390b
Isaacfv2404 <jose.fallasvalenciano@ucr.ac.cr>
2024-04-30 11:53:56 -0600

    Update jwt.strategy.ts

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -4,35 +4,30 @@ import { User } from "../entities/user.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { ConfigService } from "@nestjs/config";
-import {  UnauthorizedException } from "@nestjs/common";
+import { UnauthorizedException } from "@nestjs/common";
 
-import { Request } from "express";
-
-
-export class JwtStrategy extends PassportStrategy(Strategy){
+export class JwtStrategy extends PassportStrategy(Strategy) {
 
     constructor(
         @InjectRepository(User)
         private readonly userRepository: Repository<User>,
 
         configService: ConfigService
-    ){
-        super({secretOrKey: configService.get('JWT_SECRET'),
-               jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+    ) {
+        super({
+            secretOrKey: configService.get('JWT_SECRET'),
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
         });
     }
 
-    async validate(req: Request ,payload: any): Promise<User>{
-        
-        const {id} = payload;
-        const refreshToken = req.get('authorization').replace('Bearer', '').trim();
-        console.log(refreshToken);
-      
-        const user = await this.userRepository.findOneBy({id});
+    async validate(payload: any): Promise<User> {
 
-        
-        if(!user) throw new UnauthorizedException('Token invalido');
-        if(!user.isActive) throw new UnauthorizedException('El usuario no está activo');
+        const { id } = payload;
+
+        const user = await this.userRepository.findOneBy({ id });
+
+        if (!user) throw new UnauthorizedException('Token invalido');
+        if (!user.isActive) throw new UnauthorizedException('El usuario no está activo');
 
 
         return user;
